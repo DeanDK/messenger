f6155de8bddae4773a8b3a36d5390e374f7c4f00
DeanDK <deanbozic12@gmail.com>
2018-03-27 11:44:29 +0200

    geolocation url displayed in chat

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-const { generateMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -34,8 +34,8 @@ io.on('connection', socket => {
   socket.on('createLocationMessage', coords => {
     console.log('hehe');
     io.emit(
-      'newMessage',
-      generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`)
+      'newLocationMessage',
+      generateLocationMessage('Admin', coords.latitude, coords.longitude)
     );
   });
 
